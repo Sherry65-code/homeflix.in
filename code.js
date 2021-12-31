5cddb02bbc6adeb20c49b71ceab948d3205d0e6e
Sherry65-code <70046342+Sherry65-code@users.noreply.github.com>
2021-12-31 14:31:30 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ his = document.getElementById('main').innerHTML;
 
 window.addEventListener('load' , ()=>{
 document.getElementById('loader').style= "visibility: hidden";
-// loggin();
+loggin();
 });
 
 function bottom_of_the()
@@ -12,6 +12,7 @@ function bottom_of_the()
     <video class="mainplayer" autoplay controls>
     <source src="https://sherry65-code.github.io/homeflix.source1/bottom_of_the.mp4"></source>
     </video>
+    <br>
     <br>
     <br>
     <br>
@@ -70,4 +71,57 @@ function download()
 
 {
     window.open('https://sherry65-code.github.io/legends.com');
+}
+function songs()
+{
+document.getElementById('main').innerHTML = `   <div class="banner">
+<h1>Heoric Songs</h1></div><br><br><br><br>
+<br><br>
+<button class="display" onclick="bottom_of_the()">
+    <img src="./images/MISSIO - Bottom to deep Sea.png" alt="" class="left">
+    <button class="ans">
+        MISSIO - Bottom of the deep blue sea (Youtube)
+    </button>
+            </button>
+
+
+
+
+            <br><br>
+<button class="display" onclick="kaise_hua()">
+<img src="./images/kaise hua.png" alt="" class="left">
+<button class="ans">
+Kaise Hua - Vishal Mishra (Youtube)        </button>
+        </button>
+
+
+
+<!-- Songs start -->
+
+    <br><br>
+<button class="display" onclick="higher()">
+<img src="./images/higher.png" alt="" class="left">
+<button class="ans">
+Lematire - Higher (Youtube)      </button>
+</button>
+<!-- End -->
+
+
+
+
+
+<br><br>
+<button class="display" onclick="trailer1()">
+<img src="./images/trailer1.png" alt="" class="left">
+<button class="ans">
+HomeFlix - Trailer - Coming Soon     </button>
+        </button>
+
+<br><br>
+<button class="display" onclick="download()">
+<img src="https://wallpapercave.com/wp/wp5313405.jpg" alt="" class="left">
+<button class="ans">
+Invento Racing Legends - Click To Download Now     </button>
+    </button>
+`;
 }
